228788db5f2ea08e83044646b58c444667399157
Francis Badasu <francisbadasu@Superman-MacBook-Pro.local>
2018-10-15 11:25:42 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import "./App.css";
+import Contacts from "./components/contacts/Contacts";
+import Header from "./components/layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "./context";
+import AddContact from "./components/contacts/AddContact";
+import EditContact from "./components/contacts/EditContact";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <Router basename={process.env.PUBLIC_URL}>
+          <div className="App">
+            <Header branding="Contact Manager" />
+
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
+                <Route exact path="/About" component={About} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+export default App;
