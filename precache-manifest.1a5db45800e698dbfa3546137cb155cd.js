2f6da8e0515a7790adb0e3ad934c448e3f294069
Francis Badasu <francisbadasu@Superman-MacBook-Pro.local>
2018-10-15 11:33:19 +0000

    Updates

---
+++ precache-manifest.1a5db45800e698dbfa3546137cb155cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9c7142bbfeec04feca1",
+    "url": "/contactManager/static/js/runtime~main.c9c7142b.js"
+  },
+  {
+    "revision": "f8d998dd3c2fd56d3dc2",
+    "url": "/contactManager/static/js/main.f8d998dd.chunk.js"
+  },
+  {
+    "revision": "26de8377c830f43cc7bf",
+    "url": "/contactManager/static/js/1.26de8377.chunk.js"
+  },
+  {
+    "revision": "f8d998dd3c2fd56d3dc2",
+    "url": "/contactManager/static/css/main.3b74b38f.chunk.css"
+  },
+  {
+    "revision": "26de8377c830f43cc7bf",
+    "url": "/contactManager/static/css/1.0c1fe168.chunk.css"
+  },
+  {
+    "revision": "228f70d2598874ea1538732f33e0290c",
+    "url": "/contactManager/index.html"
+  }
+];
